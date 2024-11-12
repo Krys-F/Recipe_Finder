3e44770f5c9d7bc2f83e17796214ddf71ccad020
Krys-F <krystianfpa@gmail.com>
2024-11-13 00:01:28 +0100

    API call

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('test');
+
+
+const fetchAPI = async (query) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+
+  try{
+    const response = await fetch(url);
+    if(!response.ok) {
+      throw new Error('failed to fetch data');
+    } else {
+      const data = response.json();
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+    // Error message in container with recipes
+  }
+}
+
+fetchAPI('chicken')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
